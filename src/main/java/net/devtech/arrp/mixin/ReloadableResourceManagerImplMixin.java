2d362f88a6b69f22830567aa76fb2ed91997ba00
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2022-03-03 09:56:46 +0800

    更新

--- src/main/java/net/devtech/arrp/mixin/ReloadableResourceManagerImplMixin.java
+++ src/main/java/net/devtech/arrp/mixin/ReloadableResourceManagerImplMixin.java
@@ -1,54 +1,33 @@
 package net.devtech.arrp.mixin;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-
 import net.devtech.arrp.api.RRPCallback;
-import org.apache.logging.log4j.Logger;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
+import net.minecraft.resource.LifecycledResourceManagerImpl;
 import net.minecraft.resource.ReloadableResourceManagerImpl;
 import net.minecraft.resource.ResourcePack;
-import net.minecraft.resource.ResourceReloadMonitor;
-import net.minecraft.util.Unit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin (ReloadableResourceManagerImpl.class)
+import java.util.ArrayList;
+import java.util.List;
+
+@Mixin (LifecycledResourceManagerImpl.class)
 public abstract class ReloadableResourceManagerImplMixin {
-	@Shadow @Final private static Logger LOGGER;
+  private static final Logger LOGGER = LoggerFactory.getLogger(ReloadableResourceManagerImpl.class);
 
-	@Inject (method = "beginMonitoredReload",
-			at = @At (value = "INVOKE", target = "Ljava/util/List;iterator()Ljava/util/Iterator;"))
-	private void registerARRPs(Executor prepareExecutor,
-			Executor applyExecutor,
-			CompletableFuture<Unit> initialStage,
-			List<ResourcePack> packs,
-			CallbackInfoReturnable<ResourceReloadMonitor> cir) {
+	@ModifyVariable(method = "<init>",
+			at = @At (value = "HEAD"),index = 2, argsOnly = true)
+	private static List registerARRPs(List value) {
 		LOGGER.info("ARRP register - before vanilla");
 		List<ResourcePack> pack = new ArrayList<>();
 		RRPCallback.BEFORE_VANILLA.invoker().insert(pack);
-		pack.forEach(this::addPack);
-	}
-
-	@Inject (method = "beginMonitoredReload",
-			at = @At(value = "RETURN", shift = At.Shift.BEFORE))
-	private void registerARRPsAfter(Executor prepareExecutor,
-			Executor applyExecutor,
-			CompletableFuture<Unit> initialStage,
-			List<ResourcePack> packs,
-			CallbackInfoReturnable<ResourceReloadMonitor> cir) {
-		LOGGER.info("ARRP register - after vanilla");
-		List<ResourcePack> pack = new ArrayList<>();
-		RRPCallback.AFTER_VANILLA.invoker().insert(pack);
-		pack.forEach(this::addPack);
-	}
-
-	@Shadow
-	public abstract void addPack(ResourcePack resourcePack);
+    pack.addAll(value);
+    LOGGER.info("ARRP register - after vanilla");
+    List<ResourcePack> pack2 = new ArrayList<>();
+    RRPCallback.AFTER_VANILLA.invoker().insert(pack2);
+    pack.addAll(pack2);
+    return pack;
+  }
 }
