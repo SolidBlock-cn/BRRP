8091bbd9ad7632e3c4c89c501c050533e46961b9
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2022-03-20 19:02:02 +0800

    try using LifecycledResourceManagerImplMixin

---
+++ src/main/java/net/devtech/arrp/mixin/LifecycledResourceManagerImplMixin.java
@@ -0,0 +1,41 @@
+package net.devtech.arrp.mixin;
+
+import net.devtech.arrp.ARRP;
+import net.devtech.arrp.api.RRPCallback;
+import net.minecraft.resource.LifecycledResourceManagerImpl;
+import net.minecraft.resource.ResourcePack;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+@Mixin(LifecycledResourceManagerImpl.class)
+public abstract class LifecycledResourceManagerImplMixin {
+  private static final Logger ARRP_LOGGER = LogManager.getLogger("ARRP/ReloadableResourceManagerImplMixin");
+
+  @ModifyVariable(method = "<init>",
+      at = @At(value = "HEAD"),
+      argsOnly = true)
+  private static List<ResourcePack> registerARRPs(List<ResourcePack> packs) throws ExecutionException, InterruptedException {
+    ARRP.waitForPregen();
+
+    ARRP_LOGGER.info("ARRP register - before vanilla");
+    List<ResourcePack> before = new ArrayList<>();
+    RRPCallback.BEFORE_VANILLA.invoker().insert(before);
+
+    before.addAll(packs);
+
+    ARRP_LOGGER.info("ARRP register - after vanilla");
+    List<ResourcePack> after = new ArrayList<>();
+    RRPCallback.AFTER_VANILLA.invoker().insert(after);
+
+    before.addAll(after);
+
+    return before;
+  }
+}
