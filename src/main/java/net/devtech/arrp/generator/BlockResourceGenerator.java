ebdf2341ebf1b1b2548b01ec3466657ebb2971bf
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2022-04-30 15:10:29 +0800

    修复关于配方与进度 id 的问题

--- src/main/java/net/devtech/arrp/generator/BlockResourceGenerator.java
+++ src/main/java/net/devtech/arrp/generator/BlockResourceGenerator.java
@@ -283,7 +283,7 @@ public interface BlockResourceGenerator extends ItemResourceGenerator {
       if (stonecuttingRecipe != null) {
         final Identifier stonecuttingRecipeId = getStonecuttingRecipeId();
         pack.addRecipe(stonecuttingRecipeId, stonecuttingRecipe);
-        pack.addRecipeAdvancement(getAdvancementIdForRecipe(stonecuttingRecipeId), stonecuttingRecipe);
+        pack.addRecipeAdvancement(stonecuttingRecipeId, getAdvancementIdForRecipe(stonecuttingRecipeId), stonecuttingRecipe);
       }
     }
   }
