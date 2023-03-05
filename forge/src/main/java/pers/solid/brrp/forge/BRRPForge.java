3fcb7f1548f808565fee201d0c1b240d95e68811
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2023-03-05 08:22:50 +0800

    修复与Replay Mod兼容性的问题

--- forge/src/main/java/pers/solid/brrp/forge/BRRPForge.java
+++ forge/src/main/java/pers/solid/brrp/forge/BRRPForge.java
@@ -2,17 +2,17 @@ package pers.solid.brrp.forge;
 
 import net.devtech.arrp.BRRPDevelopment;
 import net.devtech.arrp.api.RRPInitEvent;
+import net.minecraft.block.Block;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
+import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.fml.loading.FMLLoader;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegisterEvent;
 
 @Mod("better_runtime_resource_pack")
 public class BRRPForge {
@@ -20,13 +20,12 @@ public class BRRPForge {
     DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () ->
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::particleFactoryRegister));
     if (!FMLLoader.isProduction()) {
-      FMLJavaModLoadingContext.get().getModEventBus().addListener((RegisterEvent event) -> event.register(ForgeRegistries.Keys.BLOCKS, helper -> BRRPDevelopment.registerPacks()));
+      FMLJavaModLoadingContext.get().getModEventBus().addGenericListener(Block.class, (RegistryEvent<Block> event) -> BRRPDevelopment.registerPacks());
     }
   }
 
-  @SuppressWarnings("deprecation")
   @OnlyIn(Dist.CLIENT)
-  private void particleFactoryRegister(RegisterParticleProvidersEvent event) {
+  private void particleFactoryRegister(ParticleFactoryRegisterEvent event) {
     if (FMLEnvironment.dist.isClient()) {
       ModLoader.get().postEvent(new RRPInitEvent());
     }
--- forge/src/main/java/pers/solid/brrp/forge/PlatformBridgeImpl.java
+++ forge/src/main/java/pers/solid/brrp/forge/PlatformBridgeImpl.java
@@ -1,6 +1,5 @@
 package pers.solid.brrp.forge;
 
-import net.devtech.arrp.api.RRPCallbackForge;
 import net.devtech.arrp.api.RRPEvent;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
@@ -8,14 +7,9 @@ import net.minecraft.resource.ResourcePack;
 import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.fml.ModLoader;
 import net.minecraftforge.fml.event.IModBusEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLConfig;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -24,7 +18,6 @@ import pers.solid.brrp.PlatformBridge;
 
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Objects;
 
 @ApiStatus.AvailableSince("0.8.1")
 public class PlatformBridgeImpl extends PlatformBridge {
@@ -33,28 +26,20 @@ public class PlatformBridgeImpl extends PlatformBridge {
 
   public static final PlatformBridgeImpl INSTANCE = new PlatformBridgeImpl();
 
-  @SuppressWarnings("deprecation")
   @Override
   public void postBefore(ResourceType type, List<ResourcePack> packs) {
     RRPEvent.BeforeVanilla beforeVanilla = new RRPEvent.BeforeVanilla(packs, type);
     if (ModLoader.isLoadingStateValid()) {
       ModLoader.get().postEvent((Event & IModBusEvent) beforeVanilla);
     }
-
-    // This is already deprecated.
-    RRPCallbackForge.BEFORE_VANILLA.build().stream().map(f -> f.apply(type)).filter(Objects::nonNull).forEach(packs::add);
   }
 
-  @SuppressWarnings("deprecation")
   @Override
   public void postAfter(ResourceType type, List<ResourcePack> packs) {
     RRPEvent.AfterVanilla afterVanilla = new RRPEvent.AfterVanilla(packs, type);
     if (ModLoader.isLoadingStateValid()) {
       ModLoader.get().postEvent((Event & IModBusEvent) afterVanilla);
     }
-
-    // This is already deprecated.
-    RRPCallbackForge.AFTER_VANILLA.build().stream().map(f -> f.apply(type)).filter(Objects::nonNull).forEach(packs::add);
   }
 
   @Override
