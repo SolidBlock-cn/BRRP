516a9df745468cbadda9b399dc391392eded6721
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2023-06-13 10:39:49 +0800

    更新至 1.20.1

--- fabric/src/main/java/pers/solid/brrp/v1/fabric/RRPModMenu.java
+++ fabric/src/main/java/pers/solid/brrp/v1/fabric/RRPModMenu.java
@@ -1,13 +1,15 @@
 package pers.solid.brrp.v1.fabric;
 
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import pers.solid.brrp.v1.gui.RRPConfigScreen;
 
 @Environment(EnvType.CLIENT)
-public class RRPModMenu /*implements ModMenuApi*/ {
-//  @Override
-//  public ConfigScreenFactory<?> getModConfigScreenFactory() {
-//    return RRPConfigScreen::new;
-//  }
-  // TODO: 2023/5/12, 012 complete after Mod Menu has made
+public class RRPModMenu implements ModMenuApi {
+  @Override
+  public ConfigScreenFactory<?> getModConfigScreenFactory() {
+    return RRPConfigScreen::new;
+  }
 }
