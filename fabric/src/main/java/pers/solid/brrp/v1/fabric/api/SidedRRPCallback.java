fdd8071b194220b45ac4bade6dd4046d5c7268a7
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2023-06-21 15:49:07 +0800

    improve gradle's build

--- fabric/src/main/java/pers/solid/brrp/v1/fabric/api/SidedRRPCallback.java
+++ fabric/src/main/java/pers/solid/brrp/v1/fabric/api/SidedRRPCallback.java
@@ -4,7 +4,6 @@ import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
 import net.minecraft.resource.ResourcePack;
 import net.minecraft.resource.ResourceType;
-import org.jetbrains.annotations.ApiStatus;
 import pers.solid.brrp.v1.RRPEventHelper;
 
 import java.util.List;
@@ -30,7 +29,11 @@ public interface SidedRRPCallback {
    */
   Event<SidedRRPCallback> BEFORE_VANILLA = EventFactory.createArrayBacked(SidedRRPCallback.class, CALLBACK_FUNCTION);
 
-  @ApiStatus.Experimental
+  /**
+   * Register your resource pack at a higher priority than minecraft and mod resources, but lower priority than user resources. The resources will be recognized in the Resource Pack / Data Pack screen and the {@code /data} command.
+   *
+   * @see RRPEventHelper#BEFORE_USER
+   */
   Event<SidedRRPCallback> BEFORE_USER = EventFactory.createArrayBacked(SidedRRPCallback.class, CALLBACK_FUNCTION);
 
   /**
