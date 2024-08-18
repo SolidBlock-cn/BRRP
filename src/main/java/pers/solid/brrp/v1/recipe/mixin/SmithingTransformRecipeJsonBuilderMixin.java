2ce6bfb2f6085163134d16efacd4198bb9821950
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2024-08-18 17:39:23 +0800

    Update Gradle

--- src/main/java/pers/solid/brrp/v1/recipe/mixin/SmithingTransformRecipeJsonBuilderMixin.java
+++ src/main/java/pers/solid/brrp/v1/recipe/mixin/SmithingTransformRecipeJsonBuilderMixin.java
@@ -1,70 +1,1 @@
-package pers.solid.brrp.v1.recipe.mixin;
-
-import net.minecraft.advancement.criterion.CriterionConditions;
-import net.minecraft.data.server.recipe.SmithingTransformRecipeJsonBuilder;
-import net.minecraft.recipe.book.RecipeCategory;
-import net.minecraft.util.Identifier;
-import org.apache.commons.lang3.StringUtils;
-import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import pers.solid.brrp.v1.recipe.SmithingTransformRecipeJsonBuilderExtension;
-
-@Mixin(SmithingTransformRecipeJsonBuilder.class)
-public abstract class SmithingTransformRecipeJsonBuilderMixin implements SmithingTransformRecipeJsonBuilderExtension {
-
-  @Shadow
-  public abstract SmithingTransformRecipeJsonBuilder criterion(String name, CriterionConditions conditions);
-
-  private boolean bypassesValidation;
-  private @Nullable String customRecipeCategory;
-
-  @SuppressWarnings("DataFlowIssue")
-  private SmithingTransformRecipeJsonBuilder self() {
-    return (SmithingTransformRecipeJsonBuilder) (Object) this;
-  }
-
-  @Override
-  public SmithingTransformRecipeJsonBuilder criterionMethodBridge(String criterionName, CriterionConditions criterionConditions) {
-    return criterion(criterionName, criterionConditions);
-  }
-
-  @Override
-  public SmithingTransformRecipeJsonBuilder setBypassesValidation(boolean bypassesValidation) {
-    this.bypassesValidation = bypassesValidation;
-    return self();
-  }
-
-  @Inject(method = "validate", at = @At("HEAD"), cancellable = true)
-  private void bypassValidation(Identifier recipeId, CallbackInfo ci) {
-    if (bypassesValidation) {
-      ci.cancel();
-    }
-  }
-
-  @Override
-  public SmithingTransformRecipeJsonBuilder setCustomRecipeCategory(@Nullable String recipeCategory) {
-    this.customRecipeCategory = recipeCategory;
-    return self();
-  }
-
-  @Redirect(method = "offerTo(Ljava/util/function/Consumer;Lnet/minecraft/util/Identifier;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/recipe/book/RecipeCategory;getName()Ljava/lang/String;"))
-  public String redirectGetName(RecipeCategory instance) {
-    if (customRecipeCategory != null) {
-      return customRecipeCategory;
-    } else {
-      return instance.getName();
-    }
-  }
-
-  @ModifyArg(method = "offerTo(Ljava/util/function/Consumer;Lnet/minecraft/util/Identifier;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Identifier;withPrefixedPath(Ljava/lang/String;)Lnet/minecraft/util/Identifier;"), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/recipe/book/RecipeCategory;getName()Ljava/lang/String;")))
-  public String redundantSlash(String path) {
-    if (customRecipeCategory != null && customRecipeCategory.isEmpty()) {
-      return StringUtils.replaceOnce(path, "recipes//", "recipes/");
-    } else {
-      return path;
-    }
-  }
-}
+package pers.solid.brrp.v1.recipe.mixin;import net.minecraft.advancement.criterion.CriterionConditions;import net.minecraft.data.server.recipe.SmithingTransformRecipeJsonBuilder;import net.minecraft.recipe.book.RecipeCategory;import net.minecraft.util.Identifier;import org.apache.commons.lang3.StringUtils;import org.jetbrains.annotations.Nullable;import org.spongepowered.asm.mixin.Mixin;import org.spongepowered.asm.mixin.Shadow;import org.spongepowered.asm.mixin.injection.*;import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;import pers.solid.brrp.v1.recipe.SmithingTransformRecipeJsonBuilderExtension;@Mixin(SmithingTransformRecipeJsonBuilder.class)public abstract class SmithingTransformRecipeJsonBuilderMixin implements SmithingTransformRecipeJsonBuilderExtension {  @Shadow  public abstract SmithingTransformRecipeJsonBuilder criterion(String name, CriterionConditions conditions);  private boolean bypassesValidation;  private @Nullable String customRecipeCategory;  @SuppressWarnings("DataFlowIssue")  private SmithingTransformRecipeJsonBuilder self() {    return (SmithingTransformRecipeJsonBuilder) (Object) this;  }  @Override  public SmithingTransformRecipeJsonBuilder criterionMethodBridge(String criterionName, CriterionConditions criterionConditions) {    return criterion(criterionName, criterionConditions);  }  @Override  public SmithingTransformRecipeJsonBuilder setBypassesValidation(boolean bypassesValidation) {    this.bypassesValidation = bypassesValidation;    return self();  }  @Inject(method = "validate", at = @At("HEAD"), cancellable = true)  private void bypassValidation(Identifier recipeId, CallbackInfo ci) {    if (bypassesValidation) {      ci.cancel();    }  }  @Override  public SmithingTransformRecipeJsonBuilder setCustomRecipeCategory(@Nullable String recipeCategory) {    this.customRecipeCategory = recipeCategory;    return self();  }  @Redirect(method = "offerTo(Ljava/util/function/Consumer;Lnet/minecraft/util/Identifier;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/recipe/book/RecipeCategory;getName()Ljava/lang/String;"))  public String redirectGetName(RecipeCategory instance) {    if (customRecipeCategory != null) {      return customRecipeCategory;    } else {      return instance.getName();    }  }  @ModifyArg(method = "offerTo(Ljava/util/function/Consumer;Lnet/minecraft/util/Identifier;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Identifier;withPrefixedPath(Ljava/lang/String;)Lnet/minecraft/util/Identifier;"), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/recipe/book/RecipeCategory;getName()Ljava/lang/String;")))  public String redundantSlash(String path) {    if (customRecipeCategory != null && customRecipeCategory.isEmpty()) {      return StringUtils.replaceOnce(path, "recipes//", "recipes/");    } else {      return path;    }  }}
